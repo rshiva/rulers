adc18fdf3f32c6eacabec5433af53a5724edf864
Shiva kumar <nadarshiva.r@gmail.com>
2020-08-13 11:27:49 +0530

    chapter 2 complete

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,31 @@
 require "rulers/version"
+require "rulers/routing"
 
 
 module Rulers
   class Application 
     def call(env)
-      `echo App debug > debug.text`
-        [200, {'Content-Type' =>'text/html'},
-          ["Hello from Ruby on Rulers from the gem"]
-        ]
+      if env["PATH_INFO"] == '/favicon.ico'
+        [404,{'Content-Type'=> 'text/html'},[]]
+      elsif env["PATH_INFO"] == '/'
+        [200,{'Content-Type'=>"text/html"}, [File.read(File.join(File.dirname(__FILE__), "public/index.html"))]]
+      else
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(act)
+        [200,{'Content-Type'=> 'text/html'},[text]]
+      #   [404, {'Content-Type'=> 'text/html'},["Record Not Found "]]
+      end
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env =  env
+    end
+
+    def env
+      @env
     end
   end
 end
